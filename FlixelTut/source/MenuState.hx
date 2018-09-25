07f07b0bc44bdfcdf2e3f3c50e3b27e3efe68f74
Eric Partridge <partre@rpi.edu>
2018-09-25 17:27:17 -0400

    added title screen code

--- FlixelTut/source/MenuState.hx
+++ FlixelTut/source/MenuState.hx
@@ -3,15 +3,20 @@ package;
 import flixel.FlxState;
 import flixel.ui.FlxButton;
 import flixel.FlxG;
+import flixel.FlxSprite;
 
 class MenuState extends FlxState
 {
 	var _playButton: FlxButton;
+	var _titleScreen : FlxSprite = new FlxSprite();
 	
 	override public function create():Void
 	{
+		_titleScreen.loadGraphic("assets/images/Title_Screen.png", false, 640, 480);
+		add(_titleScreen);
 		_playButton = new FlxButton(0, 0, "Play!", clickPlay);
-		_playButton.screenCenter();
+		_playButton.x = 530;
+		_playButton.y = 440;
 		add(_playButton);
 		super.create();
 	}
